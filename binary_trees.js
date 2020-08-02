26f320a94dd85ea501e1286720f1e1251be8ba8b
123JackCole <123jack.cole@gmail.com>
2020-08-01 20:38:09 -0700

    added breadth first search method onto our binary search tree

--- binary_trees.js
+++ binary_trees.js
@@ -1,53 +1,45 @@
 function Node(data) {
-    this.data = data;
-    this.left = null;
-    this.right = null;
-}
-
-function TreeNode(data) {
-    this.data = data;
-    this.children = [];
+  this.data = data;
+  this.left = null;
+  this.right = null;
 }
 
 function BinaryTree() {
-    this.root = null;
+  this.root = null;
 }
 
-BinaryTree.prototype.add = function(data) {
-    // create our node using our node constructor
-    let newNode = new Node(data);
+BinaryTree.prototype.add = function (data) {
+  // create node using node constructor
+  let newNode = new Node(data);
 
-    // set the root to the new node if there is no root
-    if (this.root === null) {
-        this.root = newNode;
-    // otherwise add our node to the tree
+  // set the root to the new node if there is no root
+  if (this.root === null) {
+    this.root = newNode;
+    // otherwise add node to the tree
+  } else {
+    this.addNode(this.root, newNode);
+  }
+};
+
+BinaryTree.prototype.addNode = function (node, newNode) {
+  // if the data is less than the node, we either fill the left side
+  // or recursively use the function again until we find an open spot
+  // for the new node
+  if (newNode.data < node.data) {
+    if (node.left === null) {
+      node.left = newNode;
     } else {
-        this.addNode(this.root, newNode);
+      this.addNode(node.left, newNode);
     }
-
-}
-
-BinaryTree.prototype.addNode = function(node, newNode) {
-
-    // if the data is less than the node, we either fill the left side
-    // or recursively use the function again until we find an open spot
-    // for the new node
-    if (newNode.data < node.data) {
-        if (node.left === null) {
-            node.left = newNode;
-        } else {
-            this.addNode(node.left, newNode);
-        }
     // do the same but on the right if the data is greater
+  } else {
+    if (node.right === null) {
+      node.right = newNode;
     } else {
-        if (node.right === null) {
-            node.right = newNode;
-        } else {
-            this.addNode(node.right, newNode);
-        }
+      this.addNode(node.right, newNode);
     }
-
-}
+  }
+};
 
 BinarySearchTree = new BinaryTree();
 
@@ -59,4 +51,29 @@ BinarySearchTree.add(3);
 BinarySearchTree.add(13);
 BinarySearchTree.add(17);
 
-console.log(BinarySearchTree)
+console.log(BinarySearchTree);
+
+BinaryTree.prototype.BFS = function (value) {
+  // array where we store the value of each node that we have visited
+  let visited = [];
+
+  // variable to keep track of current node
+  let current = this.root;
+
+  // queue to store all nodes we need to visit
+  let queue = [current];
+
+  while (queue.length) {
+    current = queue.shift();
+    visited.push(current.data);
+
+    if (current.left) {
+      queue.push(current.left);
+    }
+    if (current.right) {
+      queue.push(current.right);
+    }
+  }
+
+  return visited;
+};
